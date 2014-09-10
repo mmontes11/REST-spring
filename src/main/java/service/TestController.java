55d79bec20c52bc5ffd98512048926d317ac90fc
mmontes11 <martin11lrx@gmail.com>
2014-09-10 13:06:35 +0200

    Añadida operación de test

---
+++ src/main/java/service/TestController.java
@@ -0,0 +1,13 @@
+package service;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@RequestMapping("/test")
+	public Test test (){
+		return new Test();
+	}
+}
